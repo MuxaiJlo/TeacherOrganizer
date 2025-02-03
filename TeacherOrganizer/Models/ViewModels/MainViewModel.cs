d8c25a6e10227c8469ad402eb384894e9b2a3639
Misha Nazarenko <mykhailo.nazarenko@hneu.net>
2025-02-03 16:52:41 +0200

    Do some magic

--- TeacherOrganizer/Models/ViewModels/MainViewModel.cs
+++ TeacherOrganizer/Models/ViewModels/MainViewModel.cs
@@ -11,6 +11,7 @@ namespace TeacherOrganizer.Models.ViewModels
         public string LastName { get; set; }
 
         public string Email { get; set; }
+        public string Role { get; set; }
         public DateTime LoginTime { get; set; }
     }
 }
